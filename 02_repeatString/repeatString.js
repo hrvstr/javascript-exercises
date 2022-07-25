de300e313f072ba291aa53836a2207c8c10be3be
hrvstr <r.muhler@me.com>
2022-07-25 15:18:10 +0200

    Repeat a string a given amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else if (num === 0) {
+    return "";
+  } else if (num === 1) {
+    return string;
+  } else if (num > 1) {
+    let result = string;
+    for (let i = 0; i < num - 1; i++) {
+      result += string;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
