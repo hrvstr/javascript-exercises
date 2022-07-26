96cbc5716364755cbbaebf249dee45364ae9c270
hrvstr <r.muhler@me.com>
2022-07-26 10:57:10 +0200

    Convert temp from F to C and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+function checkIfInteger(num) {
+  if (Number.isInteger(num)) {
+    return num;
+  } else {
+    return parseFloat(num.toFixed(1));
+  }
+}
 
+const ftoc = function (degree) {
+  let result = (degree - 32) * (5 / 9);
+  return checkIfInteger(result);
 };
 
-const ctof = function() {
-
+const ctof = function (degree) {
+  let result = degree * (9 / 5) + 32;
+  return checkIfInteger(result);
 };
 
 // Do not edit below this line
