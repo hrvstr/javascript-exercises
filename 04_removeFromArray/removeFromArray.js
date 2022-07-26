7f7cc3d7940f67d92d63ea2705606dca48af2412
hrvstr <r.muhler@me.com>
2022-07-26 09:42:39 +0200

    Remove multiple items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+  if (Array.isArray(array)) {
+    if (arguments.length <= 1) {
+      return array;
+    } else if (arguments.length === 2) {
+      array.splice(array.indexOf(arguments[1]), 1);
+    } else if (arguments.length > 2) {
+      for (let i = 1; i < arguments.length; i++) {
+        if (array.includes(arguments[i])) {
+          array.splice(array.indexOf(arguments[i]), 1);
+        }
+      }
+    }
+    return array;
+  } else {
+    return "ERROR! First arg is not an array";
+  }
 };
 
 // Do not edit below this line
