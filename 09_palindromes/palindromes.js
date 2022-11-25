c7978fd5d68c105d8f600f66761aced4dbab3e40
hrvstr <r.muhler@me.com>
2022-11-25 16:40:15 +0100

    Solve palindrome assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+function isLetter(char) {
+  return char.toLowerCase() != char.toUpperCase();
+}
 
+const palindromes = function (string) {
+  let stringReversed = "";
+  let stringOnlyLetters = "";
+
+  for (i = 0; i <= string.length - 1; i++) {
+    const currentChar = string.charAt(i);
+    if (isLetter(currentChar)) {
+      stringOnlyLetters += string.charAt(i);
+    }
+  }
+
+  for (i = string.length - 1; i >= 0; i--) {
+    const currentChar = string.charAt(i);
+    if (isLetter(currentChar)) {
+      stringReversed += string.charAt(i);
+    }
+  }
+  return stringOnlyLetters.toLowerCase() === stringReversed.toLowerCase();
 };
 
 // Do not edit below this line
