ead4f5fe3d3392b34271add4e1de074b56a87fb6
hrvstr <r.muhler@me.com>
2022-11-28 10:51:02 +0100

    Solve fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+  if (nth < 1) {
+    return "OOPS";
+  } else {
+    let array = [0, 1];
+    for (i = 0; i <= nth; i++) {
+      let lastNum = array[array.length - 1];
+      let penNum = array[array.length - 2];
+      array.push(lastNum + penNum);
+    }
+    return array[nth];
+  }
 };
 
 // Do not edit below this line
