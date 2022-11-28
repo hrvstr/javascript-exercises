c3fd7c248ac7bbe9ddeed7a0474f19eddcf0c497
hrvstr <r.muhler@me.com>
2022-11-28 16:05:12 +0100

    Solution for findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const date = new Date();
+  const currentYear = date.getUTCFullYear();
+  array.sort((a, b) => {
+    let aAge = (a.yearOfDeath ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+    let bAge = (b.yearOfDeath ? b.yearOfDeath : currentYear) - b.yearOfBirth;
+    if (aAge > bAge) {
+      return -1;
+    } else if (aAge < bAge) {
+      return 1;
+    }
+    return 0;
+  });
+  return array[0];
 };
 
 // Do not edit below this line
